b3a0d9bcce155e529a0e263c649c13d8e2f99a1f
Mariana Souza <marianacaldassouza@gmail.com>
2019-05-28 21:49:44 -0700

    adding a footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import '../containers/App.css';
+
+const Footer = () => {
+	return (
+		<div>
+		 	<footer>
+             
+	          <p>© Robot Friends App 2019 | By <a href="http://www.marianacaldas.com/" target="_blank" rel="noopener noreferrer">Mariana Caldas</a>
+	          </p>
+  
+    		</footer>
+
+		</div>
+
+	);
+}
+
+export default Footer;
